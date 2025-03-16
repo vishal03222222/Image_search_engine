226cc87426e787778c64ef8f37dd84c9f058c816
vishal <mgvishalctr@gmail.com>
2025-03-16 14:36:01 +0530

    COMMIT

---
+++ search_engine/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageSearchApp from './components/ImageSearchApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSearchApp/>
+    </div>
+  );
+}
+
+export default App;
